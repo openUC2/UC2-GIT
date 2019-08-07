cb64d98e4fea29743c2a55b3d025eb868fc910dd
strolch101 <nmzs@posteo.de>
2019-08-07 05:53:53 +0200

    Python 3.6 installation script added

---
+++ 02-UC2_CreateEnvironment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#set -x
+
+. ./00-UC2_Prerequisites.sh
+
+pyv="$(python -V 2>&1)"
+PYTHON_VERSION="${pyv##* }"
+echo "PYTHON_VERSION=\"$PYTHON_VERSION\"" >> 00-UC2_Prerequisites.sh
+
+echo "[UC2] Creating UC2-Python-Environment."
+conda create -n ${UC2_ENVIRONMENT_NAME} python=$PYTHON_VERSION -y
+echo "[UC2] Exporting UC2-Python-Environment. Please wait..."
+conda env export -n ${UC2_ENVIRONMENT_NAME} > UC2_Environment.yml
+echo "[UC2] Activating UC2-Environment"
+source "${USER_HOME_DIR}/berryconda${BERRYCONDA_VERSION}/bin/activate" ${UC2_ENVIRONMENT_NAME}
+#source ${USER_HOME_DIR}/berryconda${BERRYCONDA_VERSION}/envs/${UC2_ENVIRONMENT_NAME}/bin/activate
