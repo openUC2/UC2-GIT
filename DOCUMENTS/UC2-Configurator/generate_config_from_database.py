b01825188ed0e020e661b6de1d421c0f99e841f6
beniroquai <bene.d@gmx.de>
2020-07-26 20:56:54 +0200

    Cleanup, Updated UC2 configurator script

--- DOCUMENTS/UC2-Configurator/generate_config_from_database.py
+++ DOCUMENTS/UC2-Configurator/generate_config_from_database.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jul  8 07:23:47 2020
@@ -9,22 +8,7 @@ Created on Wed Jul  8 07:23:47 2020
 from pandas import read_excel
 import pandas as pd
 import string
-# convert global class-structure into JSON
-myjsonobj = json.loads(json.dumps(my_application.__dict__, default=lambda o: '<not serializable>'))
-
-# replace the non-serializable modulels
-for iiter in range(len(myjsonobj['modulelist'])):
-    # replace modules
-    myjsonobj['modulelist'][iiter] = json.loads(json.dumps(my_application.modulelist[iiter].__dict__, default=lambda o: '<not serializable>'))
-    
-    # replace all parts inside modules 
-    for jiter in range(len(myjsonobj['modulelist'][iiter]['partslist'])):
-        
-        print(str(iiter)+'/'+str(jiter))
-        
-        myjsonobj['modulelist'][iiter]['partslist'][jiter] = json.loads(json.dumps(myjsonobj['modulelist'][iiter]['partslist'][jiter])) #.__dict__
-
-
+import json as json
 
 
 
@@ -102,6 +86,7 @@ ucs_database_filename = 'UC2_ReadyToUse_Boxes_Modules_Parts.xlsx' # change it to
 #%% Define entries in Database
 alphabet=string.ascii_lowercase
 
+is_debug = False
 # need to be lower case! 
 col_first_app = 'k' # chr(ord('K'))
 col_last_app = 'v'
@@ -134,7 +119,7 @@ all_modules_indices = [i_module for i_module, x in enumerate(worksheet.col(col_a
 all_modules = []
 i_module = 0
 for row_module in (all_modules_indices):
-    #%% row_module=6; i_module=0 # debug
+    #% row_module=6; i_module=0 # debug
     module_name = worksheet.cell(row_module, col_assembly_index+1).value
     module_githublink = worksheet.cell(row_module+1, col_assembly_index+1).value
     module_price = worksheet.cell(row_module+2, col_assembly_index+1).value
@@ -196,11 +181,29 @@ for row_module in (all_modules_indices):
 # first iterate over all applications from K...V
 #for i_application in col_all_app:
 
-#%% DEBUG: We want to have only one application to see if it works
-if(True):
-    i_application=10
-    
+'''
+DEBUG: We want to have only one application to see if it works
+
+We also want to build a json file directly! 
+'''
+
+#%%
+# export to JSON
+# Hierachy:
+#   Application    
+#   |
+#   --->Modules
+#       |
+#       Module 1 ---> Parts ----> Part 1
+#                          |
+#                          |----> Part 2
+
+
+
+for i_application in range(10,100):
+
     # read application properties
+    if i_application >= worksheet.ncols: break
     application_name = worksheet.cell(row_app_name-1, i_application).value
     application_imagelink = worksheet.cell(row_app_imagelink-1, i_application).value
     application_description = worksheet.cell(row_app_briefdescription-1, i_application).value
@@ -213,140 +216,123 @@ if(True):
                                      application_githublink, 
                                      application_imagelink, 
                                      application_price)
+
+    # create json from class and fill it with stuff
+    my_application_json = json.loads(json.dumps(my_application.__dict__, default=lambda o: '<not serializable>'))
+
     
     # now we need to fuse applications and modules
     # 1.) - we need to go through each module and check how many times we need that 
     module_iterator = 0
-    try:
-        for i_module in all_modules_indices:
-            n_add = int(worksheet.cell(i_module, i_application).value)
-         
-            # 2.) - if this module is part of the application, add it! 
-            for i_add in range(n_add):
-                my_application.addmodule(all_modules[module_iterator]) # make sure to add n-modules     
+
+    for i_module in all_modules_indices:
+        my_cellvalue = worksheet.cell(i_module, i_application).value
+        if my_cellvalue == '': break # scan for last value in cell
+        n_modules = int(my_cellvalue)
+
+        
+        # 2.) - if this module is part of the application, add it! 
+        if(is_debug): print('adding module '+str(i_module)+'  '+str(n_modules)+'-times')
+        for i_add in range(n_modules):
+            my_application.addmodule(all_modules[module_iterator]) # make sure to add n-modules   
+            my_application_json['modulelist'].append(json.loads(json.dumps(all_modules[module_iterator].__dict__, default=lambda o: '<not serializable>')))
+            
+            # add all parts
+            n_parts = len(my_application_json['modulelist'][-1]['partslist'])
+            my_partslist = []
+            for i_part in range(n_parts):
+                my_partslist.append(my_application.modulelist[-1].__dict__['partslist'][i_part].__dict__)
+
+
+            my_application_json['modulelist'][-1]['partslist']=json.loads(json.dumps(my_partslist))
+
                 
-            # just some iterator
-            module_iterator += 1            
-    except:
-        print('Error, but we are probably done...')
+                
+                
+        # just some iterator
+        module_iterator += 1            
         
     # 3.) Test if this works 
     my_application.print()
+   
+    # 4.) Save this! 
+    my_root = './GITHUBROOT' 
+    my_approot = '/APPLICATIONS'
+    my_appprefix = '/APP_'
+    my_appnpath = application_name
+    my_jsonpath = my_root+my_approot+my_appprefix+my_appnpath
+    print('should save to: '+my_jsonpath)
 
-#%%
-#https://stackoverflow.com/questions/5160077/encoding-nested-python-object-in-json
-import json
-
-
-# export to JSON
-# Hierachy:
-#   Application    
-#   |
-#   --->Modules
-#       |
-#       Module 1 ---> Parts ----> Part 1
-#                          |
-#                          |----> Part 2
-
-
-# convert global class-structure into JSON
-myjsonobj = json.loads(json.dumps(my_application.__dict__, default=lambda o: '<not serializable>'))
-
-# replace the non-serializable modulels
-for iiter in range(len(myjsonobj['modulelist'])):
-    # replace modules
-    myjsonobj['modulelist'][iiter] = my_application.modulelist[iiter].__dict__ #, default=lambda o: '<not serializable>') 
-    
-    # replace all parts inside modules 
-    for jiter in range(len(myjsonobj['modulelist'][iiter]['partslist'])):
-        
-        print(str(iiter)+'/'+str(jiter))
-        
-        try:
-            myjsonobj['modulelist'][iiter]['partslist'][jiter] = json.dumps(myjsonobj['modulelist'][iiter]['partslist'][jiter].__dict__)
-        except:
-            myjsonobj['modulelist'][iiter]['partslist'][jiter] = json.dumps(myjsonobj['modulelist'][iiter]['partslist'][jiter])
-        
-#%%
-    
-
-json_string = json.dumps(myjsonobj)
-    
-# %% Now transform it to compatible JSON code using code from :
-#https://github.com/AlecVercruysse/UC2-GIT/blob/master/APPLICATIONS/APP_Incubator_Microscope/config.json
-#https://github.com/AlecVercruysse/UC2-GIT/blob/master/CAD/config_generator.py
-#%%
-import os, json
-
-
-
-
-already_written = ["ASSEMBLY_Baseplate_v2", "ASSEMBLY_CUBE_LED_Matrix_v2", "ASSEMBLY_CUBE_Mirror_45_v2", "ASSEMBLY_CUBE_Dichroic_Beamsplitter_v2", "ASSEMBLY_CUBE_Base_v2", "ASSEMBLY_CUBE_Z-STAGE_v2"]
-dirs = [x for x in os.scandir() if ("ASSEMBLY" in x.name)]
-dirs = [x for x in dirs if x.name not in already_written]
-print(os.getcwd())
-os.chdir(input("enter chdir path: ('.' to stay in current folder). we want to end up in /CAD of the repo we're generating configs for:\n"))
-for d in dirs:
-    print("generating config for {}".format(d.name))
-    desc = input("Description (one liner):\n")
-    
-    print("We'll now guide you through setting up the options. enter an option, or press return when done.")
-    options = {}
-    selectingOptions = True
-    while (selectingOptions):
-        key = input("new option (camelCase'd):\n")
-        if key == "":
-            selectingOptions = False
-        else:
-            displayName = input("enter the name of the option as shown to the user:\n")
-            choices = []
-            for i in range(int(input("number of choices:\n"))):
-                choices.append(input("choice #{}:\n".format(i+1)))
-                options[key] = {
-                    "displayName": displayName,
-                    "choices": choices
-                }
-            
-    print("Time to assign files to options. For all files, please input the path relative to the home dir of the module.\n e.g.: STL/file.stl")
-            
-    print("\n fixed files (that will always be included with the module): enter path, or return when complete")
-    fixedFiles = []
-    inputtingFixedFiles = True
-    while (inputtingFixedFiles):
-        path = input("path:\n")
-        if path == "":
-            inputtingFixedFiles = False
-        else:
-            fixedFiles.append(path)
-            
-    print("For each file that is included conditionally, specify the name of the files, and we'll ask for the conditions of inclusion. Again, return ends the list.")
-    dynamicFiles = []
-    inputtingDynamicFiles = True
-    while (inputtingDynamicFiles):
-        f = {}
-        path = input("path:\n")
-        if path == "":
-            inputtingDynamicFiles = False
-        else:
-            f["path"] = path
-            f["conditions"] = {}
-            for key in options.keys():
-                choices = input("enter a comma (no spaces!!) delimited list of all choices to include this file for. press return if this file is not dependent on the option:.\n for option:\t{}\n".format(key)).split(',')
-                if choices != "":
-                    f["conditions"][key] = choices
-            dynamicFiles.append(f)
-                    
-                    
-        
-    with open(d.name + "/config.json", "w") as j:
-        data = {
-            "type":"module",
-            "description" : desc,
-            "options": options,
-            "fixedFiles": fixedFiles,
-            "dynamicFiles": dynamicFiles
-        }
-        json.dump(data, j, indent=2)
+    with open('config.json', "w") as j:
+        json.dump(my_application_json, j, indent=4)
     print("config written!\n\n")
     
+
     
+
+
+
+
+# #%%
+# import os, json
+
+
+
+
+# already_written = ["ASSEMBLY_Baseplate_v2", "ASSEMBLY_CUBE_LED_Matrix_v2", "ASSEMBLY_CUBE_Mirror_45_v2", "ASSEMBLY_CUBE_Dichroic_Beamsplitter_v2", "ASSEMBLY_CUBE_Base_v2", "ASSEMBLY_CUBE_Z-STAGE_v2"]
+# dirs = [x for x in os.scandir() if ("ASSEMBLY" in x.name)]
+# dirs = [x for x in dirs if x.name not in already_written]
+# print(os.getcwd())
+# os.chdir(input("enter chdir path: ('.' to stay in current folder). we want to end up in /CAD of the repo we're generating configs for:\n"))
+# for d in dirs:
+#     print("generating config for {}".format(d.name))
+#     desc = input("Description (one liner):\n")
+    
+#     print("We'll now guide you through setting up the options. enter an option, or press return when done.")
+#     options = {}
+#     selectingOptions = True
+#     while (selectingOptions):
+#         key = input("new option (camelCase'd):\n")
+#         if key == "":
+#             selectingOptions = False
+#         else:
+#             displayName = input("enter the name of the option as shown to the user:\n")
+#             choices = []
+#             for i in range(int(input("number of choices:\n"))):
+#                 choices.append(input("choice #{}:\n".format(i+1)))
+#                 options[key] = {
+#                     "displayName": displayName,
+#                     "choices": choices
+#                 }
+            
+#     print("Time to assign files to options. For all files, please input the path relative to the home dir of the module.\n e.g.: STL/file.stl")
+            
+#     print("\n fixed files (that will always be included with the module): enter path, or return when complete")
+#     fixedFiles = []
+#     inputtingFixedFiles = True
+#     while (inputtingFixedFiles):
+#         path = input("path:\n")
+#         if path == "":
+#             inputtingFixedFiles = False
+#         else:
+#             fixedFiles.append(path)
+            
+#     print("For each file that is included conditionally, specify the name of the files, and we'll ask for the conditions of inclusion. Again, return ends the list.")
+#     dynamicFiles = []
+#     inputtingDynamicFiles = True
+#     while (inputtingDynamicFiles):
+#         f = {}
+#         path = input("path:\n")
+#         if path == "":
+#             inputtingDynamicFiles = False
+#         else:
+#             f["path"] = path
+#             f["conditions"] = {}
+#             for key in options.keys():
+#                 choices = input("enter a comma (no spaces!!) delimited list of all choices to include this file for. press return if this file is not dependent on the option:.\n for option:\t{}\n".format(key)).split(',')
+#                 if choices != "":
+#                     f["conditions"][key] = choices
+#             dynamicFiles.append(f)
+                    
+                    
+        
